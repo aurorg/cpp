071471de80d4f95848789851e16269edc579724b
aurorg <93782149+aurorg@users.noreply.github.com>
2022-01-01 09:52:46 +0800

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,105 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define TSIZE 45
-
-struct film
-{
-    char title[TSIZE];
-    int rating;
-    struct film *next;
-};
-
-void print_reverse_list(struct film *head);
-char *s_gets(char *st, int n);
+#define HMEAN(X, Y) (2.0 * (X) * (Y) / ((X) + (Y)))
 
 int main(void)
 {
-    struct film *head = NULL;
-    struct film *prev, *current;
-    char input[TSIZE];
+    double x, y, ans;
 
-    puts("Enter first movie title:");
-    while (s_gets (input, TSIZE) != NULL && input[0] != '\0')
+    printf("Enter a pair of numbers (q to quit): ");
+    while (scanf("%lf %lf", &x, &y) == 2)
     {
-        current = (struct film *)malloc(sizeof(struct film));
-        if (head == NULL)
-        {
-            head = current;
-        }
-        else
-        {
-            prev->next = current;
-        }
-        current->next = NULL;
-        strcpy(current->title, input);
-        puts("Enter your rating <0-10>:");
-        scanf("%d", &current->rating);
-        while (getchar() != '\n')
-            continue;
-        puts("Enter next movie title (empty line to stop):");
-        prev = current;
+        ans = HMEAN(x, y);
+        printf("%g = harmonic mean of %g %g.\n", ans, x, y);
+        printf("Enter a pair of numbers (q to quit): ");
     }
-
-    if (head == NULL)
-    {
-        printf("No data entered. ");
-    }
-    else
-    {
-        printf("Here is the movie list:\n");
-    }
-    current = head;
-    while (current != NULL)
-    {
-        printf("Movie: %s  Rating: %d\n",
-               current->title, current->rating);
-        current = current->next;
-    }
-    if (head != NULL)
-    {
-        printf("\nHere is the list in reverse order:\n");
-        print_reverse_list(head);
-    }
-    current = head;
-    while (current != NULL)
-    {
-        free(current);
-        current = current->next;
-    }
-    printf("Bye!\n");
+    puts("Done.");
 
     return 0;
-}
-
-void print_reverse_list(struct film *head)
-{
-    if (head->next != NULL)
-    {
-        print_reverse_list(head->next); //递归到最后一个结点后进行反向输出;
-    }
-    printf("Movie: %10s  Rating: %4d\n", head->title, head->rating);
-    return;
-}
-
-char *s_gets(char *st, int n)
-{
-    char *ret_val;
-    char *find;
-
-    ret_val = fgets(st, n, stdin);
-    if (ret_val)
-    {
-        find = strchr(st, '\n');
-        if (find)
-        {
-            *find = '\0';
-        }
-        else
-        {
-            while (getchar() != '\n')
-                continue;
-        }
-    }
-    return ret_val;
 }
