15e65826a07216f0bb2cec1cf508e116db05b5b7
aurorg <93782149+aurorg@users.noreply.github.com>
2022-01-23 21:01:28 +0800

    Add files via upload

---
+++ 6.1.c
@@ -0,0 +1,26 @@
+/*#include<stdio.h>
+int main(void)
+{
+ char letters[26];
+ int i;
+ 
+ for(i = 0;i < 26; i++)
+ {
+  letters[i] = 'a' + i;
+  printf("%c ",letters[i]);
+ }
+ 
+ return 0;
+}*/
+#include <stdio.h>
+int main(void)
+{
+    char letters[26];
+    int i;
+    for(i=0;i<26;i++)
+    {
+        letters[i]='a'+i;
+        printf("%c ",letters[i]);
+    }
+    return 0;
+}
