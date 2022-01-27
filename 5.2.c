1782186aeaac70bfd6b68b68ff38d00af50d2ac8
aurorg <93782149+aurorg@users.noreply.github.com>
2022-01-27 17:37:11 +0800

    Add files via upload

---
+++ 5.2.c
@@ -0,0 +1,29 @@
+/*#include<stdio.h>
+int main(void)
+{
+ int n, i;
+ 
+ printf("Please enter a integer.");
+ scanf("%d",&n);
+ 
+ for(i = 0;i <= 10;i++)
+ {
+   printf("%d ",n);
+   n += 1;
+ }
+ 
+ return 0;
+}*/
+#include <stdio.h>
+int main(void)
+{
+  int n,i;
+  printf("please enter a integer.");
+  scanf("%d",&n);
+  for(i=0;i<=10;i++)
+  {
+    printf("%d  ",n);
+    n+=1;
+  }
+  return 0;
+}
