8ab85b7aa318867f3fd16bc9eacc48973e1874c9
aurorg <93782149+aurorg@users.noreply.github.com>
2022-01-28 11:11:42 +0800

    Add files via upload

---
+++ 4.1.c
@@ -0,0 +1,22 @@
+/*#include<stdio.h>
+int main(void)
+{
+ char first_name[20],last_name[20];
+ printf("Please input your first_name.\n");
+ scanf("%s",first_name);
+ printf("Please input your last_name.\n");
+ scanf("%s",last_name);
+ printf("%s , %s\n",first_name,last_name);
+ return 0;
+} */
+#include <stdio.h>
+int main(void)
+{
+    char first_name[20],last_name[20];
+    printf("Please input your first_name.\n");
+    scanf("%s",first_name);
+    printf("Please input your last_name.\n");
+    scanf("%s",last_name);
+    printf("%s , %s\n",first_name,last_name);
+    return 0;
+}      
