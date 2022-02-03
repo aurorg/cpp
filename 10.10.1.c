a3cfe60a0fee2f26008ae202cdc0bf6e6ce4d03e
aurorg <93782149+aurorg@users.noreply.github.com>
2022-02-03 23:32:50 +0800

    Add files via upload

---
+++ 10.10.1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int f(int n)
+{    
+if(n==1||n==2)     
+    return 1;   
+else       
+    return f(n-1)+f(n-2);
+}
+int main()
+{   
+   int i;   
+   scanf("%d",&i);   
+   printf("%d",f(i));    
+   return 0;
+   }
