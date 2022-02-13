07d37a635b0c4026b3efe6fd60b243f6658e6670
aurorg <93782149+aurorg@users.noreply.github.com>
2022-02-13 23:25:22 +0800

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,90 +1,80 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#define LEN 12
-
-struct month
+/*#include <stdio.h>
+typedef struct stu
 {
-    char name[10];
-    char abbrev[4];
-    int days;
-    int monumb;
-};
-
-const struct month months[LEN] =
+    char name[20];
+    short age;
+    char tele[12];
+    char sex[5];
+}stu;
+int main()
 {
-    {"January", "Jan", 31, 1},
-    {"February", "Feb", 28, 2},
-    {"March", "Mar", 31, 3},
-    {"April", "Apr", 30, 4},
-    {"May", "May", 31, 5},
-    {"June", "Jun", 30, 6},
-    {"July", "Jul", 31, 7},
-    {"August", "Aug", 31, 8},
-    {"September", "Sep", 30, 9},
-    {"October", "Oct", 31, 10},
-    {"November", "Nov", 30, 11},
-    {"December", "Dec", 31, 12}
-};
-
-int days(char *name);
-
-int main(void)
-{
-    int daytotal;
-    char input[LEN];
-
-    printf("Please enter the name of a month (q to quit): ");
-    while (scanf("%11s", input) == 1 && input[0] != 'q')
-    {
-        daytotal = days(input);
-        if (daytotal > 0)
-        {
-            printf("There are %d days through %s.\n", daytotal, input);
-        }
-        else
-        {
-            printf("%s isn't a valid month!\n", input);
-        }
-        printf("You can enter again (q to quit): ");
-        while (getchar() != '\n')
-            continue;
-    }
-    printf("Done.\n");
-
+    stu s1={"张三",20,"12345678911","男"};
+    struct stu s2={"xxx",30,"1234321123","女"};
     return 0;
-}
-
-int days(char *name)
+}*/
+/*结构体的初始化
+#include <stdio.h>
+struct s
 {
-    int i = 1;
-    int num = 0;
-    int total = 0;
+    int a;
+    char c;
+    char arr[20];
+    double d;
+};
 
-    name[0] = toupper(name[0]);
-    while (name[i] != '\0')
-    {
-        name[i] = tolower(name[i]);
-        i++;
-    }
-    for (i = 0; i < LEN; i++)
-    {
-        if (0 == strcmp(name, months[i].name)) //查找用户输入的月份名是否存在;
-        {
-            num = months[i].monumb;
-            break;
-        }
-    }
-    if (num == 0)
-    {
-        total = -1; //用户输入不合法则总天数置为-1;
-    }
-    else
-    {
-        for (i = 0; i < num; i++)
-        {
-            total += months[i].days; //加至用户输入月份的天数;
-        }
-    }
-    return total;
+struct t
+{
+    char ch[10];
+    struct s $;
+    char*pc;
+};
+int main()
+{
+    char arr[]="hello\n";
+    struct t t={"hehe",{100,'w',"hello world",5.20},arr};//结构体的定义以及初始化
+    printf("%s\n",t.ch);
+    printf("%s\n",t.$.arr);
+    printf("%lf\n",t.$.d);
+    printf("%s\n",t.pc);
+    return 0;
+}*/
+/*#include <stdio.h>
+typedef struct stu
+{
+    char name[20];
+    short age;
+    char tele[12];
+    char sex[5];
+}stu;
+
+void Print1(stu tmp)//使用void表示没有返回值
+{
+    printf("name:%s\n",tmp.name);
+    printf("age:%d\n",tmp.age);
+    printf("tele:%s\n",tmp.tele);
+    printf("sex:%s\n",tmp.sex);
 }
+
+void Print2(stu*ps)//传结构体的地址，*ps是结构体指针
+{
+    printf("name:%s\n",ps->name);
+    printf("age:%d\n",ps->age);
+    printf("tele:%s\n",ps->tele);
+    printf("sex:%s\n",ps->sex);
+    
+}
+int main()
+{
+    stu s ={"李四",40,"1532657154","男"};
+    //打印结构体数据的两种方式
+    //两个函数哪个好
+    //2好，时间短，空间消耗少
+    //【重点】函数传参的时候，参数是需要压栈的。如果传递一个结构体对象的时候，结构体过大，参数压栈的系统开销比较大，所以会导致性能的下降
+    //【结构体传参的时候，要传结构体的地址】
+    Print1(s);
+    Print2(&s);//传地址
+    return 0;
+}*/
+//【栈区】：局部变量，函数的参数形式，函数调用也开辟空间
+//【堆区】：动态内存分配，malloc/free,realloc,calloc,
+//【静态区】：全局变量，静态变量
